d10ecfd05d6a64f7b9f7074c9e4077f5a9003716
fayadchet <chettaouifayad@gmail.com>
2017-02-13 16:53:26 +0100

    Ajout de 3 composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+//Pour pouvoir utiliser le ngModel,il faut ajouter la class FormsModule
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent }  from './components/mainCmp/app.component';
+import { DashboardComponent }  from './components/dashboard/dashboard.component';
+import { EditStudentComponent }  from './components/edit.student/edit.student.component';
+import { AddStudentComponent }  from    './components/add.student/add.student.component';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  //Ajouter les class dans le tableau des imports
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, DashboardComponent,EditStudentComponent, AddStudentComponent, ],
   bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
