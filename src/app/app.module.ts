4cd16a77862e2e3f288557f44b1c97814bcb7702
fayadchet <chettaouifayad@gmail.com>
2017-02-14 13:00:00 +0100

    Ajout de 3 composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,43 @@
+
+//Importer les class pour configurer le module
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http'
 
+//Importer le module des routes
+import { Routing } from './routing';
+
+//Importer le composant principal
 import { AppComponent } from './app.component';
+
+//Importer les composants de l'application (pages)
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { EditComponent } from './components/edit/edit.component';
-import { AddComponent } from './components/add-student/add/add.component';
-import { AddStudentComponent } from './components/add-student/add-student.component';
 import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { HeaderComponent } from './directives/header/header.component';
+import { FooterComponent } from './directives/footer/footer.component';
 
 @NgModule({
+  //Ajouter tous les composants dans le tableau des déclarations
   declarations: [
     AppComponent,
     DashboardComponent,
-    EditComponent,
-    AddComponent,
-    DashboardComponent,
+    EditStudentComponent,
     AddStudentComponent,
-    EditStudentComponent
+    HeaderComponent,
+    FooterComponent
   ],
+  //Ajouter les modules dans le tableau des imports
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    //Ajouter la class des routes
+    Routing
   ],
   providers: [],
+  //Ajouter le composant principal dans le tableau bootstrap
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/routing.ts
@@ -0,0 +1,49 @@
+// Importer les class nécessaires à l'utilisation des routes
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+//Importer les composants à utiliser dans les routes
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+
+//Déclarer une constante de type Routes = variable non modifiable
+const appRoutes: Routes = [
+
+    //Définition des routes 
+    {
+
+        //Redirection de la page d'accueil
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+
+
+    {
+        //Définir l'adresse 
+        path: 'dashboard',
+        //Définir le composant à utiliser
+        component: DashboardComponent
+    },
+
+    {
+        //Définir l'adresse 
+        path: 'add-student',
+        //Définir le composant à utiliser
+        component: AddStudentComponent
+    },
+
+    {
+        //Définir l'adresse 
+        path: 'edit-student',
+        //Définir le composant à utiliser
+        component: EditStudentComponent
+    }
+
+
+];
+
+//Exporter la constante des routes
+export const Routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+
