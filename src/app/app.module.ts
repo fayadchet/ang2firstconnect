47882789a6d835b9145525f21414d1d1ea98e620
fayadchet <chettaouifayad@gmail.com>
2017-02-14 10:56:14 +0100

    Ajout de 3 composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EditComponent } from './components/edit/edit.component';
+import { AddComponent } from './components/add-student/add/add.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    EditComponent,
+    AddComponent,
+    DashboardComponent,
+    AddStudentComponent,
+    EditStudentComponent
   ],
   imports: [
     BrowserModule,
