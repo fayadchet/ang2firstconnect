e39e84f13f679e57882df140865f8f121a99267a
fayadchet <chettaouifayad@gmail.com>
2017-02-14 10:06:02 +0100

    Ajout d'angular CLI

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { FolderPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('folder App', function() {
+  let page: FolderPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new FolderPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
-import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-//Pour pouvoir utiliser le ngModel,il faut ajouter la class FormsModule
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent }  from './components/mainCmp/app.component';
-import { DashboardComponent }  from './components/dashboard/dashboard.component';
-import { EditStudentComponent }  from './components/edit.student/edit.student.component';
-import { AddStudentComponent }  from    './components/add.student/add.student.component';
-
-
+import { AppComponent } from './app.component';
 
 @NgModule({
-  //Ajouter les class dans le tableau des imports
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, DashboardComponent,EditStudentComponent, AddStudentComponent, ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-
 export class AppModule { }
